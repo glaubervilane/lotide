ea19d8265e8e8bea807913cb0ed7a09595bf5b08
Glauber Marques <glauberfmarques@gmail.com>
2023-04-24 21:42:23 -0600

    Launching middle function to get elements from senter of an array

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = function(array) {
+  const length = array.length;
+  if (length <= 2) {
+    return [];
+  }
+  if (length % 2 === 0) {
+    const midIndex = length / 2 - 1;
+    return [array[midIndex], array[midIndex + 1]];
+  } else {
+    const midIndex = Math.floor(length / 2);
+    return [array[midIndex]];
+  }
+};
+
+// TEST CODE
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
