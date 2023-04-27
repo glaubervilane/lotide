397ed0a98eb1287df4dd69b11bbcb9bc61417b2b
Glauber Marques <glauberfmarques@gmail.com>
2023-04-26 20:32:24 -0600

    Iplementing letterPosition functionality

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== ' ') {
+      if (!results[letter]) {
+        results[letter] = [];
+      }
+      results[letter].push(i);
+    }
+  }
+  return results;
+};
+
+
+const phrase = "hello";
+const result1 = letterPositions(phrase);
+
+assertArraysEqual(result1["h"], [0]); // should pass
+assertArraysEqual(result1["e"], [1]); // should fail
+assertArraysEqual(result1["l"], [2, 3]); // should fail
+assertArraysEqual(result1["o"], [4]); // should fail
