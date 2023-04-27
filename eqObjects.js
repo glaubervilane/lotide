dd22de0ce8c83af4f367d2b6cd44ab3172b66bb8
Glauber Marques <glauberfmarques@gmail.com>
2023-04-27 06:28:40 -0600

    Updating eqObjects to treat arrays nested and primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,14 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -16,8 +27,12 @@ const eqObjects = function(object1, object2) {
     return false;
   }
 
-  for (const key of obj1Keys) {
-    if (object1[key] !== object2[key]) {
+  for (const key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
@@ -31,3 +46,10 @@ assertEqual(eqObjects(shirtObject , anotherShirtObject), true); // => true
 
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false); // => false
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject , anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject , longSleeveMultiColorShirtObject), false); // => false
