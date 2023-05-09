92d57c18ebc9b8cf5b86971e9662e87c71651926
Glauber Marques <glauberfmarques@gmail.com>
2023-05-09 06:13:22 -0600

    Refactoring code into Lotide project sharing files

--- head.js
+++ head.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
@@ -5,17 +7,4 @@ const head = function(arr) {
   return arr[0];
 };
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+module.exports = head;
